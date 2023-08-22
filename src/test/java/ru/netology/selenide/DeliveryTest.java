0bd4d561e52a01996509b373fb14958b6f8d0464
Helga test <www.helga@mail.ru>
2023-08-22 09:26:06 +0300

    test passed

--- src/test/java/ru/netology/selenide/DeliveryTest.java
+++ src/test/java/ru/netology/selenide/DeliveryTest.java
@@ -30,49 +30,49 @@ public class DeliveryTest {
     }
 
     @Test
-    void successfulOrderCardTest() {
+    void successOrderCard() {
 
         $("[data-test-id='city'] [placeholder='Город']").setValue("Москва");
         $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(3));
-        $("[data-test-id='name'] [name='name']").setValue("Иванов Иван");
-        $("[data-test-id='phone'] [name='phone']").setValue("+79850000000");
+        $("[data-test-id='name'] [name='name']").setValue("Карл Маркс");
+        $("[data-test-id='phone'] [name='phone']").setValue("+79111111111");
         $("[data-test-id='agreement']").click();
         $(".button__content").click();
         $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + dateGenerator(3)), Duration.ofSeconds(15)).shouldBe(visible);
 
     }
     @Test
-    void plannedDatePlusTenDaysTest() {
+    void DatePlusFiveDays() {
 
         $("[data-test-id='city'] [placeholder='Город']").setValue("Москва");
-        $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(10));
-        $("[data-test-id='name'] [name='name']").setValue("Иванов Иван");
-        $("[data-test-id='phone'] [name='phone']").setValue("+79850000000");
-        $("[data-test-id='agreement']").click();
-        $(".button__content").click();
-        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + dateGenerator(10)), Duration.ofSeconds(15)).shouldBe(visible);
-
-    }
-    @Test
-    void plannedDatePlusTwoDaysTest() {
-
-        $("[data-test-id='city'] [placeholder='Город']").setValue("Москва");
-        $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(2));
-        $("[data-test-id='name'] [name='name']").setValue("Иванов Иван");
-        $("[data-test-id='phone'] [name='phone']").setValue("+79850000000");
-        $("[data-test-id='agreement']").click();
-        $(".button__content").click();
-        $$(".input__sub").find(Condition.text("Заказ на выбранную дату не возможен"));
-
-    }
-
-    @Test
-    void cityNotOnTheListOfAdministrativeCentersTest() {
-
-        $("[data-test-id='city'] [placeholder='Город']").setValue("Балашиха");
         $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(5));
-        $("[data-test-id='name'] [name='name']").setValue("Иванов Иван");
-        $("[data-test-id='phone'] [name='phone']").setValue("+79850000000");
+        $("[data-test-id='name'] [name='name']").setValue("Карл Маркс");
+        $("[data-test-id='phone'] [name='phone']").setValue("+79111111111");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + dateGenerator(5)), Duration.ofSeconds(15)).shouldBe(visible);
+
+    }
+    @Test
+    void datePlusOneDays() {
+
+        $("[data-test-id='city'] [placeholder='Город']").setValue("Москва");
+        $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(1));
+        $("[data-test-id='name'] [name='name']").setValue("Карл Маркс");
+        $("[data-test-id='phone'] [name='phone']").setValue("+79111111111");
+        $("[data-test-id='agreement']").click();
+        $(".button__content").click();
+        $$(".input__sub").find(Condition.text("Неверная дата"));
+
+    }
+
+    @Test
+    void cityNotInList() {
+
+        $("[data-test-id='city'] [placeholder='Город']").setValue("Гатчина");
+        $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(5));
+        $("[data-test-id='name'] [name='name']").setValue("Карл Маркс");
+        $("[data-test-id='phone'] [name='phone']").setValue("+79111111111");
         $("[data-test-id='agreement']").click();
         $(".button__content").click();
         $(".input__sub").shouldHave(Condition.text("Доставка в выбранный город недоступна"));
@@ -80,45 +80,35 @@ public class DeliveryTest {
     }
 
     @Test
-    void nameInEnglishTest() {
+    void nameInEnglish() {
         $("[data-test-id='city'] [placeholder='Город']").setValue("Москва");
         $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(5));
-        $("[data-test-id='name'] [name='name']").setValue("Иванов Ivan");
-        $("[data-test-id='phone'] [name='phone']").setValue("+79850000000");
+        $("[data-test-id='name'] [name='name']").setValue("Karl Marks");
+        $("[data-test-id='phone'] [name='phone']").setValue("+79111111111");
         $("[data-test-id='agreement']").click();
         $(".button__content").click();
         $$(".input__inner").find(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
 
     }
     @Test
-    void shorPhoneNumberTest (){
-        $("[data-test-id='city'] [placeholder='Город']").setValue("Барнаул");
+    void shortPhoneNumber (){
+        $("[data-test-id='city'] [placeholder='Город']").setValue("Москва");
         $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(5));
-        $("[data-test-id='name'] [name='name']").setValue("Иванов Иван");
-        $("[data-test-id='phone'] [name='phone']").setValue("+7985000000");
+        $("[data-test-id='name'] [name='name']").setValue("Карл Маркс");
+        $("[data-test-id='phone'] [name='phone']").setValue("+7911111111");
         $("[data-test-id='agreement']").click();
         $(".button__content").click();
         $$(".input__inner").find(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
     @Test
-    void notCheckboxTest (){
-        $("[data-test-id='city'] [placeholder='Город']").setValue("Барнаул");
+    void notAgreement (){
+        $("[data-test-id='city'] [placeholder='Город']").setValue("Москва");
         $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(5));
-        $("[data-test-id='name'] [name='name']").setValue("Иванов Иван");
-        $("[data-test-id='phone'] [name='phone']").setValue("+7985000000");
+        $("[data-test-id='name'] [name='name']").setValue("Карл Маркс");
+        $("[data-test-id='phone'] [name='phone']").setValue("+79111111111");
         $(".button__content").click();
         $$(".checkbox__text").find(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
 
     }
-    @Test
-    void cityNotTest() {
-
-        $("[data-test-id='city'] [placeholder='Город']").setValue("");
-        $("[data-test-id='date'] [placeholder='Дата встречи']").val(dateGenerator(5));
-        $("[data-test-id='name'] [name='name']").setValue("Иванов Иван");
-        $("[data-test-id='phone'] [name='phone']").setValue("+79850000000");
-        $("[data-test-id='agreement']").click();
-        $(".button__content").click();
-        $(".input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
-    }
+    
     }
